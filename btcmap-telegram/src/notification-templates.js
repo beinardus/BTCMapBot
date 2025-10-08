8adea099097d40e37aa882e3c4e4e42bcd282857
beinardus <46429579+beinardus@users.noreply.github.com>
2025-10-09 00:03:00 +0200

    compact links hide id

--- btcmap-telegram/src/notification-templates.js
+++ btcmap-telegram/src/notification-templates.js
@@ -1,33 +1,33 @@
 import {t, WELCOME, GOODBYE} from "translation";
 
-const createWelcomeUrl = ({type, id}) =>
-  `<a href="https://btcmap.org/merchant/${type}:${id}">${id}</a>`;
+const createWelcomeUrl = (innerText, {type, id}) =>
+  `<a href="https://btcmap.org/merchant/${type}:${id}">${innerText}</a>`;
 
-const createFarewellUrl = ({type, id}) =>
-  `<a href="https://www.openstreetmap.org/${type}/${id}">${id}</a>`;
+const createFarewellUrl = (innerText, {type, id}) =>
+  `<a href="https://www.openstreetmap.org/${type}/${id}">${innerText}</a>`;
 
 const createWelcomeNotificationText = (props) => {
   const {name,user:{language}} = props;
-  return `${t(language, WELCOME)}: ${name} (${createWelcomeUrl(props)})`;
+  return `${t(language, WELCOME)}: ${createWelcomeUrl(name, props)}`;
 };
 
 const createFarewellNotificationText = (props) => {
   const {name,user:{language}} = props;
-  return `${t(language, GOODBYE)}: ${name} (${createFarewellUrl(props)})`;
+  return `${t(language, GOODBYE)}: ${createFarewellUrl(name, props)}`;
 };
 
 const createWelcomeNotificationFloodText = (props) => {
   const {name, city: propCity, geo: {city: geoCity, town: geoTown, village: village, country_code}} = props;
   const town = propCity || geoCity || geoTown || village;
-
-  return `${name}, ${town} (${country_code}) ${createWelcomeUrl(props)}`;
+  const summary = `${name}, ${town} (${country_code})`;
+  return createWelcomeUrl(summary, props);
 };
     
 const createFarewellNotificationFloodText = (props) => {
   const {name, city: propCity, geo: {city: geoCity, town: geoTown, village: village, country_code}} = props;
   const town = propCity || geoCity || geoTown || village;
-
-  return `${name}, ${town} (${country_code}) ${createFarewellUrl(props)}`;
+  const summary = `${name}, ${town} (${country_code})`;
+  return  createFarewellUrl(summary, props);
 };
 
 export { createWelcomeNotificationText, createFarewellNotificationText, createWelcomeNotificationFloodText, createFarewellNotificationFloodText}
